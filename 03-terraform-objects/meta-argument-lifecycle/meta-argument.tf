2632f6c11179b0df63885b13d16ea6e3be373f6f
kalyaneswar <kalyaneswarreddy16696@gmail.com>
2025-07-03 12:38:50 +0530

    terraform

---
+++ 03-terraform-objects/meta-argument-lifecycle/meta-argument.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region     = "us-east-1"
+}
+
+resource "aws_instance" "myec2" {
+    ami = "ami-0f34c5ae932e6f0e4"
+    instance_type = "t2.micro"
+
+    tags = {
+        Name = "HelloEarth"
+    }
+
+# This is ignore even if we manual change tags during plan/apply
+    lifecycle {
+        ignore_changes = [tags]
+    }
+}
